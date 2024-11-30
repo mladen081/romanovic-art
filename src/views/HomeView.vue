3ae12e6211791e527d7bc79db9e491485d10833a
mladen081 <jovicmladen094@gmail.com>
2024-11-30 22:12:27 +0100

    008 added mini animation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,16 @@
   <h1>romanovic art</h1>
   <p v-if="isAdmin">logged in as admin</p>
   <p v-if="isUser">logged in as {{ user }}</p>
+
+  <div class="slide" v-if="isUser">
+    <p class="slide-text">welcome</p>
+
+    <p class="slide-text-r">more or less</p>
+
+    <p class="slide-text">than</p>
+
+    <p class="slide-text-r">art</p>
+  </div>
 </template>
 
 <script setup>
@@ -20,4 +30,37 @@ const isUser = computed(() => authStore.role === 'ROLE_USER')
 const user = authStore.user
 </script>
 
-<style scoped></style>
+<style scoped>
+.slide {
+  margin: 5rem 0;
+  width: 300px;
+}
+
+.slide-text {
+  position: relative;
+  animation: slide-l 5s ease-out forwards;
+}
+
+.slide-text-r {
+  position: relative;
+  animation: slide-r 5s ease-out forwards;
+}
+
+@keyframes slide-l {
+  from {
+    transform: translateX(-100%);
+  }
+  to {
+    transform: translateX(0);
+  }
+}
+
+@keyframes slide-r {
+  from {
+    transform: translateX(100%);
+  }
+  to {
+    transform: translateX(0);
+  }
+}
+</style>
