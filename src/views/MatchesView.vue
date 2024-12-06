8661983721308e6a5e9214e18ebc79b162272080
mladen081 <jovicmladen094@gmail.com>
2024-12-07 00:39:53 +0100

    021 block delete match without admin password

--- src/views/MatchesView.vue
+++ src/views/MatchesView.vue
@@ -23,10 +23,25 @@
         <div v-if="isAdmin" class="delete-icon">
           <i
             class="fa-regular fa-trash"
-            @click="removeMatch(match.id)"
+            @click="openDeleteModal(match.id)"
             style="cursor: pointer; font-size: 2rem"
           ></i>
         </div>
+      </div>
+    </div>
+
+    <div v-if="isModalOpen" class="modal">
+      <div class="modal-content">
+        <label for="admin-password">confirm admin password</label>
+        <input
+          v-model="adminPassword"
+          name="admin-password"
+          id="admin-password"
+          type="password"
+          placeholder="enter password"
+        />
+        <button @click="confirmPassword">confirm</button>
+        <button @click="closeModal">cancel</button>
       </div>
     </div>
   </div>
@@ -39,6 +54,9 @@ import { useAuthStore } from '../stores/authStore'
 
 const authStore = useAuthStore()
 const matches = ref([])
+const isModalOpen = ref(false)
+const adminPassword = ref('')
+const selectedMatchId = ref(null)
 
 onMounted(async () => {
   await authStore.checkLogin()
@@ -56,34 +74,56 @@ const listMatches = async () => {
   }
 }
 
+const openDeleteModal = (matchId) => {
+  selectedMatchId.value = matchId
+  isModalOpen.value = true
+}
+
+const closeModal = () => {
+  isModalOpen.value = false
+  adminPassword.value = ''
+}
+
+const confirmPassword = async () => {
+  const isPasswordCorrect = await validateAdminPassword(adminPassword.value)
+
+  if (isPasswordCorrect) {
+    await removeMatch(selectedMatchId.value)
+    closeModal()
+  } else {
+    alert('Incorrect password!')
+  }
+}
+
+const validateAdminPassword = async (password) => {
+  return password === 'tata'
+}
+
 const removeMatch = async (matchId) => {
   try {
     await deleteMatch(matchId)
     console.log('successfully deleted match')
     listMatches()
   } catch (error) {
-    console.error('Error deleting match:', error)
+    console.error('error deleting match:', error)
   }
 }
 </script>
 
 <style scoped>
 .matches-container {
-  padding: 20px;
-  overflow-x: hidden; /* Sprečava horizontalno pomeranje */
+  overflow-x: hidden;
 }
 
 .matches-grid {
   display: grid;
-  grid-template-columns: repeat(7, 1fr); /* 7 kolona za svaki detalj meča */
+  grid-template-columns: repeat(7, 1fr);
   gap: 10px;
   grid-template-areas: 'opponents attendance date ha result scorers position delete';
-  border-collapse: collapse;
-  box-sizing: border-box; /* Uključuje padding i border u ukupnu širinu i visinu */
 }
 
 .grid-header {
-  display: contents; /* Ovo omogućava da radi kao zaglavlje tabele */
+  display: contents;
   font-weight: bold;
 }
 
@@ -108,7 +148,26 @@ const removeMatch = async (matchId) => {
   padding: 10px;
 }
 
-/* Media query za ekrane ispod 767px (još manji ekran) */
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 9999;
+}
+
+.modal-content {
+  background-color: white;
+  padding: 20px;
+  border-radius: 2px;
+  width: 300px;
+}
+
 @media (max-width: 991px) {
   .matches-grid {
     grid-template-columns: 1fr; /* 1 kolona za najmanje ekrane */
@@ -122,7 +181,6 @@ const removeMatch = async (matchId) => {
       'position'
       'delete';
   }
-
   .grid-header {
     display: none;
   }
